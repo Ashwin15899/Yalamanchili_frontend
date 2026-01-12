b0cb92f9eeff75cbc744cf7961901be6f05da4b1
Ashwin15899 <90087340+Ashwin15899@users.noreply.github.com>
2026-01-12 16:17:42 +0530

    Add files via upload

---
+++ Quiz/login.js
@@ -0,0 +1,23 @@
+import {users} from "./DataSet/users.js";
+
+const loginForm = document.getElementById("login-form");
+const email = document.getElementById("email");
+const pwd = document.getElementById("password");
+const emailErr = document.getElementById("emailErr");
+const pwdErr = document.getElementById("pwdErr")
+
+loginForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const userFound = users.find(item=>item.Email==email.value)
+    if(userFound){
+        if(pwd.value==userFound.Password){
+            window.location.href="http://127.0.0.1:5500/Pages/test.html";
+        } else {
+            pwdErr.innerHTML="Incorrect Password"
+            pwdErr.style.color="red";
+        }
+    } else {
+        emailErr.innerHTML="No user found";
+        emailErr.style.color="red";
+    }
+})
---
+++ Quiz/questionAnswers.js
@@ -0,0 +1,52 @@
+export const qAndA = [
+    {
+        "Question": "Who is MS Dhoni?",
+        "AnswerArr": ["Doctor", "Engineer", "Cricketer", "Lawyer"],
+        "Answer": "Cricketer"
+    },
+    {
+        "Question": "How many times CSK won IPL cup?",
+        "AnswerArr": [1, 3, 5, 2],
+        "Answer": 5
+    },
+    {
+        "Question": "What is the capital of India?",
+        "AnswerArr": ["Mumbai", "New Delhi", "Chennai", "Bangalore"],
+        "Answer": "New Delhi"
+    },
+    {
+        "Question": "How many continents are there in the world?",
+        "AnswerArr": [7, 5, 8, 6],
+        "Answer": 7
+    },
+    {
+        "Question": "What is 2+2?",
+        "AnswerArr": [4, 3, 5, 2],
+        "Answer": 4
+    },
+    {
+        "Question": "What is Java?",
+        "AnswerArr": ["Scripting language", "Programming language", "Database", "Machine language"],
+        "Answer": "Programming language"
+    },
+    {
+        "Question": "How many months are there in a year?",
+        "AnswerArr": [11, 13, 15, 12],
+        "Answer": 12
+    },
+    {
+        "Question": "In which of the below given is a Javascript library?",
+        "AnswerArr": ["ReactJS", "Spring-boot", "AngularJS", "Laravel"],
+        "Answer": "ReactJS"
+    },
+    {
+        "Question": "When did India got Independence?",
+        "AnswerArr": [1942, 1943, 1947, 1950],
+        "Answer": 1947
+    },
+    {
+        "Question": "Where is USA?",
+        "AnswerArr": ["Europe", "North America", "South America", "Africa"],
+        "Answer": "North America"
+    }
+]
